019ed3f9237e6c41d1d715df627a2c5eeef157c6
Ethan Welborn <welborn.ethan@gmail.com>
2014-06-06 15:02:14 -0600

    better mission specs

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,5 +1,5 @@
 class Mission < ActiveRecord::Base
-	validates :name, presence: true
+	validates :title, presence: true
 	validates :description, presence: true
 
 	has_many :objectives
--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -2,30 +2,16 @@ require 'spec_helper'
 
 describe Mission do
 
-	it 'should be valid with all attributes' do
-		Mission.new(name: 'Example name', description: 'Example description').should be_valid
+	it 'has a valid factory' do
+		FactoryGirl.create(:mission).should be_valid
 	end
 
-	it 'should be invalid with no attributes' do
-		Mission.new().should_not be_valid
+	it 'is invalid without a title' do
+		FactoryGirl.build(:mission, title: nil).should_not be_valid
 	end
 
-	context 'with assigned attributes' do
-
-		before(:each) do
-			@mission = Mission.new(name: 'Example name', description: 'Example description')
-		end
-
-		it 'should be invalid with a missing name' do
-			@mission.name = nil
-			@mission.should_not be_valid
-		end
-
-		it 'should be invalid with a missing description' do
-			@mission.description = nil
-			@mission.should_not be_valid
-		end
-
+	it 'is invalid without a description' do
+		FactoryGirl.build(:mission, description: nil).should_not be_valid
 	end
 
 end
