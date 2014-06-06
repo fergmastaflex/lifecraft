a57556c98069ad641135f05e5b8614aa93150fa2
fergmastaflex <joey@fergmedia.com>
2014-06-06 11:00:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifecraft::Application.config.secret_key_base = '2c1a9f4d659578befe9b89648b5648890c6859d3f5cabd8506e51299e75176d8ea9dda69966d79fffbf1b4329d62ad58e51eed73635cd33ac9815ab3225124cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lifecraft::Application.config.session_store :cookie_store, key: '_lifecraft_session'
