198dafccab36b520345fbedb2e096eee7071ced7
fergmastaflex <joey@fergmedia.com>
2014-06-06 13:35:33 -0600

    Writes spec for new campaign

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe CampaignsController do 
+  before do
+    @new_campaign = Campaign.new
+  end
 
   it 'loads index page' do
     get :index
@@ -10,6 +13,11 @@ describe CampaignsController do
   it 'loads new page' do
     get :new
     response.should render_template(:new)
+  end
+
+  it 'creates new campaign for creation' do
+    get :new
+    assigns(:campaign).should be_an_instance_of Campaign
   end
 
 end
