8879a8d91d074863f2175144268d9261a16abd82
Ethan Welborn <welborn.ethan@gmail.com>
2014-06-06 14:02:07 -0600

    adding some simple model specs

--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -1,7 +1,34 @@
-require 'test_helper'
+require 'spec_helper'
 
-class ObjectiveTest < ActiveSupport::TestCase
-	
-	subject { assigns("objective") }
-	it { should be_valid }
+describe Objective do
+
+	context 'Objective creation' do
+
+		it 'should be invalid without all the required attributes' do
+			Objective.new().should_not be_valid
+		end
+
+		it 'should be invalid if the name is empty' do
+			Objective.new(name: "").should_not be_valid
+		end
+
+		context 'with assigned attributes' do
+
+			before(:each) do
+				@objective = Objective.new(name: 'Example name', directive: 'Example Directive', start_date: DateTime.now, end_date: DateTime.now)
+			end
+
+			it 'should be valid with all assigned attributes' do
+				@objective.should be_valid
+			end
+
+			it 'should be invalid with a missing name' do
+				@objective.name = ''
+				@objective.should_not be_valid
+			end
+
+		end
+
+	end
+
 end
