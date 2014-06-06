918bff52688ff1f0a9ebdc423ab01483ba36efff
fergmastaflex <joey@fergmedia.com>
2014-06-06 14:04:29 -0600

    Adds factories

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -6,4 +6,14 @@ class CampaignsController < ApplicationController
   def new
     @campaign = Campaign.new
   end
+
+  def create
+    @campaign = Campaign.new(params[:campaign])
+
+    if @campaign.save
+      redirect_to campaigns_path, notice: "Your campaign was created"
+    else
+      render action: 'new', alert: "Your campaign was not created"
+    end
+  end
 end
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe CampaignsController do 
   before do
     @new_campaign = Campaign.new
+    @campaign = build(:campaign)
   end
 
   it 'loads index page' do
@@ -20,5 +21,9 @@ describe CampaignsController do
     assigns(:campaign).should be_an_instance_of Campaign
   end
 
+  it 'redirect after save' do
+    @campaign.save
+    response.should redirect_to campaigns_path
+  end
 end
 
